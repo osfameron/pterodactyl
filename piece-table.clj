a1fc4373c7d9445237d8e1373eba6fe442052600
Hakim Cassimally <hakim.cassimally@gmail.com>
2016-12-15 16:28:23 +0000

    initial zipper sketch

--- piece-table.clj
+++ piece-table.clj
@@ -1,90 +1,117 @@
-(defrecord Table [strings pieces])
-
 (defrecord Piece [table-index from to])
+(defn make-piece [table-index string]
+  (Piece. table-index 0 (count string)))
 
 (defn piece-length [piece] (- (:to piece) (:from piece)))
 
-(defn make-piece [string table-index]
-  (Piece. table-index 0 (count string)))
+(defn split-piece [piece at]
+  (let [length (piece-length piece)]
+    (cond 
+      (<= at 0) [piece]
+      (>= at length) [piece]
+      :else (let [pivot (+ at (:from piece))
+                  before (assoc piece :to pivot)
+                  after (assoc piece :from pivot)]
+               [before after]))))
 
+(defrecord Table [strings pieces])
 (defn make-table [strings]
   (Table. strings 
           (into '() (reverse 
-                      (map make-piece strings (range))))))
+                      (map make-piece (range) strings)))))
+
+(defn show-piece [strings piece]
+  (let [{:keys [table-index from to]} piece
+        string (nth strings table-index)]
+      (subs string from to)))
 
 (defn show-table [table]
-  (let [ strings (:strings table)
-         pieces (:pieces table)
-         show-piece (fn [piece]
-                      (let [ table-index (:table-index piece)
-                             from (:from piece)
-                             to (:to piece)
-                             string (get strings table-index)]
-                          (subs string from to)))]
-    (apply str (map show-piece pieces))))
-    
-(defn pieces-with-offset [pieces]
-  "Returns a vector like [11 [ [0 6 piece1] [6 11 piece2] ] ]
-   e.g. [length [ [from to piece1] [from to piece2] ] ]"
-  (reduce 
-    (fn [a b] 
-      (let [from (first a)
-            acc (second a)
-            to (+ from (piece-length b))
-            piece [from to b]]
-        [ to (conj acc piece)]))
-    [0 []]
-    pieces))
-
-(defn split-pieces [pieces at]
-  (let [pwo (pieces-with-offset pieces)
-        length (first pwo)
-        pwos (second pwo)
-        rough (split-with #(< (first %) at) pwos)
-        pre (into [] (first rough))
-        pivot (last pre)
-        pre' (pop pre)
-        post (second rough)
-        pivot-from (first pivot)
-        split-offset (- at pivot-from)
-        pp (last pivot)
-        table-offset (+ (:from pp) split-offset)
-        pivot-pre (Piece. (:table-index pp) (:from pp) table-offset) 
-        pivot-post (Piece. (:table-index pp) split-offset (:to pp))
-        fc (fn [a b]
-             (filter #(> (piece-length %) 0)
-               (concat a b)))]
-     [ (fc (into [] (map last pre')) [pivot-pre])
-       (fc [pivot-post] (into [] (map last post)))]))
-  
-(defn split-table [table at]
-  "Largely useless, except for testing?"
-  (Table. (:strings table) (apply concat (split-pieces (:pieces table) at)))) 
-  
-(defn table-delete [table from to]
-  "NB: from < to, or nonsensical results, TODO"
-  (let [pieces (:pieces table) 
-        pre (split-pieces pieces from)
-        post (split-pieces pieces to)
-        left (first pre)
-        right (second post)]
-    (Table. (:strings table) (concat left right))))
+  (let [{:keys [:pieces :strings]} table
+        show-piece' (partial show-piece strings)]
+    (apply str (map show-piece' pieces))))
          
+; zipper class, a finger onto the data
+; (Clojure has zippers, but they seem to be only on hierarchical data
+; structures?)
+(defrecord Dactyl [strings back pieces acc-pos curr-pos])
+(defn make-dactyl [table]
+  (Dactyl. (:strings table) '() (:pieces table) 0 0))
+
+(defn curr [dactyl]
+  (first (:pieces dactyl)))
+
+(defn curr-text [dactyl]
+  (nth (:strings dactyl) (:table-index (curr dactyl))))
+
+(defn curr-text-post [dactyl]
+  (subs (curr-text dactyl) (:curr-pos dactyl)))
+
+(defn curr-text-pre [dactyl]
+  (subs (curr-text dactyl) 0 (:curr-pos dactyl)))
+
+(defn curr-post [dactyl]
+  (- (piece-length (curr dactyl)) (:curr-pos dactyl)))
+
+(defn traverse-back [dactyl]
+  (let [{:keys [:back :pieces :acc-pos]} dactyl]
+    (if (empty? back)
+      nil
+      (let [new (first back)]
+        (assoc dactyl
+               :back (rest back)
+               :pieces (conj pieces new)
+               :acc-pos (- acc-pos (piece-length new)) 
+               :curr-pos 0)))))
+
+(defn traverse-forward [dactyl]
+  (let [{:keys [:back :pieces :acc-pos]} dactyl
+        next (rest pieces)]
+    (if (empty? next)
+      nil
+      (let [old (first pieces)]
+        (assoc dactyl
+               :back (conj back old)
+               :pieces next
+               :acc-pos (+ acc-pos (piece-length old)) 
+               :curr-pos 0)))))
+
+(defn traverse-right [dactyl count]
+  (let [dactyl (dissoc dactyl :bounce) ; assuming this will be cheap noop in
+                                       ; most cases?
+        avail (curr-post dactyl)]
+    (cond
+      (zero? count) dactyl
+      (< count avail) (update dactyl :curr-pos (partial + count))
+      :else 
+        (let [next (traverse-forward dactyl)]
+          (if (nil? next)
+            (assoc dactyl :bounce :right, :curr-pos (piece-length (curr dactyl)))
+            (recur next (- count avail)))))))
    
+(defn text-after
+  ([dactyl len] 
+   (apply str (text-after dactyl len [])))
+
+  ([dactyl len acc]
+   (cond
+     (nil? dactyl) acc
+     (zero? len) acc
+     :else
+      (let [text (curr-text-post dactyl)
+            chunk (subs text 0 (min len (count text)))]
+        (recur
+          (traverse-forward dactyl)
+          (- len (count chunk))
+          (conj acc chunk)))))) 
+     
 
 (comment
-  (concat (into [] (concat '((1)) '((2)))))
-  (pop [1 2 3])
-  (last '(1 2 3 4))
-  (into [] (list 1 2 3 4))
   (def table (make-table ["hello " "world"]))
-  (show-table user/table)
-  (> 6 (first [4 5]))
-  flatmap
-  (print [ table (split-table table 6)])
   (show-table table)
-  (show-table (split-table table 4))
-  (show-table (split-table table 6))
-  (show-table (split-table table 7))
-  (show-table 
-    (table-delete table 7 9)))
+  (def d (make-dactyl table))
+  (text-after d 100)
+  (text-after (traverse-right d 3) 100)
+  (traverse-forward (traverse-right d 1))
+  (:acc-pos (traverse-forward d))
+  (:acc-pos (traverse-back (traverse-back d)))
+  (:acc-pos (traverse-forward (traverse-forward (traverse-forward d)))))
