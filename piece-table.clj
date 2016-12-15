70113c962d771505a8710fc7781435f0eae24624
Hakim Cassimally <hakim.cassimally@gmail.com>
2016-12-15 23:30:38 +0000

    traverse-left, delete-between
    Added
     - traverse-left
     - various helper functions like dactyl-pos
     - split-dactyl (which required fixing a but in curr-text)
     - delete-between

--- piece-table.clj
+++ piece-table.clj
@@ -8,7 +8,7 @@
   (let [length (piece-length piece)]
     (cond 
       (<= at 0) [piece]
-      (>= at length) [piece]
+      (>= at length) [piece] ; but this shouldn't happen, except at end of buffer
       :else (let [pivot (+ at (:from piece))
                   before (assoc piece :to pivot)
                   after (assoc piece :from pivot)]
@@ -41,7 +41,10 @@
   (first (:pieces dactyl)))
 
 (defn curr-text [dactyl]
-  (nth (:strings dactyl) (:table-index (curr dactyl))))
+  (let [piece (curr dactyl)
+        {:keys [from to]} piece
+        {:keys [strings curr-pos]} dactyl]
+    (subs (nth strings (:table-index piece)) from to)))
 
 (defn curr-text-post [dactyl]
   (subs (curr-text dactyl) (:curr-pos dactyl)))
@@ -49,19 +52,24 @@
 (defn curr-text-pre [dactyl]
   (subs (curr-text dactyl) 0 (:curr-pos dactyl)))
 
-(defn curr-post [dactyl]
+(defn curr-pos-post [dactyl]
+  "Number of characters from curr-pos to end of piece (dual of curr-pos)"
   (- (piece-length (curr dactyl)) (:curr-pos dactyl)))
+
+(defn dactyl-pos [dactyl]
+  (+ (:acc-pos dactyl) (:curr-pos dactyl)))
 
 (defn traverse-back [dactyl]
   (let [{:keys [:back :pieces :acc-pos]} dactyl]
     (if (empty? back)
       nil
-      (let [new (first back)]
+      (let [new (first back)
+            length (piece-length new)]
         (assoc dactyl
                :back (rest back)
                :pieces (conj pieces new)
-               :acc-pos (- acc-pos (piece-length new)) 
-               :curr-pos 0)))))
+               :acc-pos (- acc-pos length)
+               :curr-pos length)))))
 
 (defn traverse-forward [dactyl]
   (let [{:keys [:back :pieces :acc-pos]} dactyl
@@ -75,10 +83,13 @@
                :acc-pos (+ acc-pos (piece-length old)) 
                :curr-pos 0)))))
 
+; For now, we'll clear the bounce flag at the beginning of a traverse
+; I'm assuming this is a noop if already not set - e.g. won't create
+; new datastructure?  In any case, this will probably move to outer
+; controller framework eventually.
 (defn traverse-right [dactyl count]
-  (let [dactyl (dissoc dactyl :bounce) ; assuming this will be cheap noop in
-                                       ; most cases?
-        avail (curr-post dactyl)]
+  (let [dactyl (dissoc dactyl :bounce) 
+        avail (curr-pos-post dactyl)]
     (cond
       (zero? count) dactyl
       (< count avail) (update dactyl :curr-pos (partial + count))
@@ -86,6 +97,18 @@
         (let [next (traverse-forward dactyl)]
           (if (nil? next)
             (assoc dactyl :bounce :right, :curr-pos (piece-length (curr dactyl)))
+            (recur next (- count avail)))))))
+
+(defn traverse-left [dactyl count]
+  (let [dactyl (dissoc dactyl :bounce) 
+        avail (:curr-pos dactyl)]
+    (cond
+      (zero? count) dactyl
+      (<= count avail) (update dactyl :curr-pos #(- % count))
+      :else 
+        (let [next (traverse-back dactyl)]
+          (if (nil? next)
+            (assoc dactyl :bounce :left, :curr-pos 0)
             (recur next (- count avail)))))))
    
 (defn text-after
@@ -103,15 +126,75 @@
           (traverse-forward dactyl)
           (- len (count chunk))
           (conj acc chunk)))))) 
+
+(defn split-dactyl [dactyl]
+  "Split the dactyl at current insertion point.  Anything to left of curr-pos will become a new piece.  Noop if we are at far-left of piece."
+  (let [{:keys [curr-pos back pieces acc-pos]} dactyl]
+    (if (zero? curr-pos)
+      dactyl
+      (let [[pre post] (split-piece (curr dactyl) curr-pos)]
+        (assoc dactyl
+               :back (conj back pre)
+               :pieces (conj (rest pieces) post)
+               :curr-pos 0
+               :acc-pos (+ acc-pos (count pre)))))))
+
+(defn delete-between [d1 d2]
+  (if (> (dactyl-pos d1) (dactyl-pos d2))
+    (delete-between d2 d1)
+    (let [d1' (split-dactyl d1)
+          d2' (split-dactyl d2)]
+      (assoc d1' :pieces (:pieces d2')))))
      
+; next steps
+  ; insert
+  ; text-before?
+  ; go to
+  ; unzip & print whole buffer
 
 (comment
   (def table (make-table ["hello " "world"]))
   (show-table table)
   (def d (make-dactyl table))
   (text-after d 100)
-  (text-after (traverse-right d 3) 100)
+  (doseq [i (range 12)] 
+    (-> d (traverse-right i)
+          (#(println (str)
+             (-> % (:curr-pos))
+             " / "
+             (-> % (curr) (:table-index))
+             " / "
+             (-> % (dactyl-pos))
+             " / "
+             (-> % (text-after 100)))))) 
+  (doseq [i (range 12)] 
+    (-> (traverse-right d 11) (traverse-left i)
+          (#(println (str)
+             (-> % (:curr-pos))
+             " / "
+             (-> % (curr) (:table-index))
+             " / "
+             (-> % (dactyl-pos))
+             " / "
+             (-> % (text-after 100)))))) 
+
+  (-> d (traverse-right 6) (text-after 100))
+  (-> d (traverse-right 3) (split-dactyl) (:curr-pos))
+  (-> d (traverse-right 3) (split-dactyl) (curr-text))
+  (-> d (traverse-right 3) (text-after 100))
+  (-> d (traverse-right 3) (split-dactyl) (text-after 100))
+  (-> d (traverse-right 10) 
+      (#(let [d1 %
+              d2 (traverse-left d1 4)]
+         (delete-between d1 d2)))
+      (traverse-left 100)
+      (text-after 100))
+
+  (-> d (traverse-right 6) (text-after 100))
+  (-> d (traverse-right 7))
+  (-> d (traverse-right 7) (traverse-left 2) (text-after 100))
+  (-> d (traverse-right 7) (traverse-left 2))
   (traverse-forward (traverse-right d 1))
   (:acc-pos (traverse-forward d))
-  (:acc-pos (traverse-back (traverse-back d)))
+  (:acc-pos (traverse-back (traverse-forward d)))
   (:acc-pos (traverse-forward (traverse-forward (traverse-forward d)))))
